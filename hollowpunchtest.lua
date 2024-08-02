f8e7b576b2cfb81bc484b46b9ecd6d06a266bf95
tamarixr <averageultrakillenjoyer@gmail.com>
2024-08-02 10:09:00 +0200

    Update hollowpunchtest.lua

--- hollowpunchtest.lua
+++ hollowpunchtest.lua
@@ -2,15 +2,19 @@ local message = Instance.new("Message")
 message.Parent = game.Workspace
 message.Text = "Cursed technique : Blue"
 wait(3)
+message:Remove()
 local message = Instance.new("Message")
 message.Parent = game.Workspace
 message.Text = "Reversal Cursed technique : Red"
 wait(3)
+message:Remove()
 local message = Instance.new("Message")
 message.Parent = game.Workspace
 message.Text = "IMAGINARY TECHNIQUE :"
-wait(1)
+wait(2)
+message:Remove()
 local message = Instance.new("Message")
 message.Parent = game.Workspace
 message.Text = "HOLLOW PURPLE!"
-
+wait(3)
+message:Remove()
