b17a6b9739a25cac276c117de35632480ad69a86
tamarixr <averageultrakillenjoyer@gmail.com>
2024-08-02 13:53:10 +0200

    Update hollowpunchtesbt.lua

--- hollowpunchtesbt.lua
+++ hollowpunchtesbt.lua
@@ -1,7 +1,7 @@
-message = "Hanten Jujutsu shiki Aka 🔴"
+message = "Jujutsi Hanten Shiki : Aka 🔴"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
 wait(1)
-message = "Hanten Jujutsu shiki Ao 🔵"
+message = "Jujitsu Hanten Shiki : Ao 🔵"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
 wait(2)
 message = "Kyoshiki... 🔵 🔴"
