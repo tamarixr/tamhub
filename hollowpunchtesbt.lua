cc4dd89a42578197bb01132602e703cd17cf01f4
tamarixr <averageultrakillenjoyer@gmail.com>
2024-08-02 16:39:20 +0200

    Update hollowpunchtesbt.lua

--- hollowpunchtesbt.lua
+++ hollowpunchtesbt.lua
@@ -1,15 +1,11 @@
-humanoid.AnimationPlayed:Connect(onAnimationPlayed)
-
-local animationId = 12983333733
-
 message = "Jujutsi Hanten Shiki : Aka 🔴"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
-wait(1)
+wait(1.5)
 message = "Jujitsu Hanten Shiki : Ao 🔵"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
-wait(2)
+wait(1.5)
 message = "Kyoshiki... 🔵 🔴"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
-wait(1)
+wait(2.5)
 message = "Murasaki! 🔮"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
