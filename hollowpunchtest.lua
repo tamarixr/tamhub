c0b8ec6e38d8b9864c7895053c413ecc3960ceb7
tamarixr <averageultrakillenjoyer@gmail.com>
2024-08-02 10:06:32 +0200

    Add files via upload

---
+++ hollowpunchtest.lua
@@ -0,0 +1,16 @@
+local message = Instance.new("Message")
+message.Parent = game.Workspace
+message.Text = "Cursed technique : Blue"
+wait(3)
+local message = Instance.new("Message")
+message.Parent = game.Workspace
+message.Text = "Reversal Cursed technique : Red"
+wait(3)
+local message = Instance.new("Message")
+message.Parent = game.Workspace
+message.Text = "IMAGINARY TECHNIQUE :"
+wait(1)
+local message = Instance.new("Message")
+message.Parent = game.Workspace
+message.Text = "HOLLOW PURPLE!"
+
