cc6e01cc3c6dcdf985a643565df17368530cea68
tamarixr <averageultrakillenjoyer@gmail.com>
2024-08-02 07:09:26 +0200

    Update antidctest.lua

--- antidctest.lua
+++ antidctest.lua
@@ -4,7 +4,7 @@ local Player = Players.LocalPlayer
 local PlayerGui = Player:WaitForChild("PlayerGui")
 
 local function DestroyGui()
-    PlayerGui:FindFirstChild("Your_Gui"):Destroy()
+    PlayerGui:FindFirstChild("InfinitiveUI"):Destroy()
 end
 
 DestroyGui()
