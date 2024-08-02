68f6bee6df84677be5d11803d95b7d51536094d3
tamarixr <averageultrakillenjoyer@gmail.com>
2024-08-02 07:08:37 +0200

    Update antidctest.lua

--- antidctest.lua
+++ antidctest.lua
@@ -1,16 +1,10 @@
-local player = game.Players.LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
-local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+local Players = game:GetService("Players")
 
-local voidPosition = Vector3.new(0, -10000, 0)
-local originalPosition = humanoidRootPart.CFrame
+local Player = Players.LocalPlayer
+local PlayerGui = Player:WaitForChild("PlayerGui")
 
-game:GetService("UserInputService").InputBegan:Connect(function(input)
-    if input.KeyCode == Enum.KeyCode.E then
-        if humanoidRootPart.CFrame == originalPosition then
-            humanoidRootPart.CFrame = CFrame.new(voidPosition)
-        else
-            humanoidRootPart.CFrame = originalPosition
-        end
-    end
+local function DestroyGui()
+    PlayerGui:FindFirstChild("Your_Gui"):Destroy()
 end
+
+DestroyGui()
