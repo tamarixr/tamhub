f86cb54ba62a96da376751e8c58372aa2ee071d6
tamarixr <averageultrakillenjoyer@gmail.com>
2024-08-02 16:59:34 +0200

    Create idkqfqs.lua

---
+++ idkqfqs.lua
@@ -0,0 +1,10 @@
+KeyActivation = ("3") --you can change 'e' to any letter you want.
+ 
+ 
+player = game.Players.LocalPlayer
+mouse = player:GetMouse()
+mouse.KeyDown:connect(function(Key)
+if (Key:lower() == KeyActivation then
+print(KeyActivation) -- loadstring(game:HttpGet("https://raw.githubusercontent.com/tamarixr/tamhub/main/gojojs.lua
+end)
+end)
