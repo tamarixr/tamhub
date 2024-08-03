2deb86f92365e518261f7ab0f6ad69e710329fde
tamarixr <averageultrakillenjoyer@gmail.com>
2024-08-03 13:45:07 +0200

    Update newandrevampedtamhub.lua

--- newandrevampedtamhub.lua
+++ newandrevampedtamhub.lua
@@ -33,18 +33,7 @@ end)
 btns:Seperator()
 
 btns:Button("Spoof id to game owner id", function()
-	Callback = function() 
-        game.Players.LocalPlayer.Character.Head:Destroy()
-        if game.CreatorType == Enum.CreatorType.User then
-            game.Players.LocalPlayer.UserId = game.CreatorId
-            end
-            
-            if game.CreatorType == Enum.CreatorType.Group then
-            game.Players.LocalPlayer.UserId = game:GetService("GroupService"):GetGroupInfoAsync(game.CreatorId).Owner.Id
-            end
-    
-    end
-}
+
 DiscordLib:Notification("Notification", "Spoofed!", "Okay!")
 end)
 
