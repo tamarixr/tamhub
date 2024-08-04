b55b05ff11d93162c62c059554b41db1d355b12c
tamari <averageultrakillenjoyer@gmail.com>
2024-08-04 23:27:58 +0200

    Create hollowmaxpunch.lua

---
+++ hollowmaxpunch.lua
@@ -0,0 +1,11 @@
+message = "Phase, Twilight, Eyes of Wisdom,"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(1.5)
+message = "Phase, Paramita, Pillars of Light,"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(1.5)
+message = "Nine ropes, Polarised light, Crow and Shomyo, The gap Between Within and Without,"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(2.75)
+message = "Hollow, Purple."
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
