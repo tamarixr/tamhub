2243939decae8c5c9087fd1b5cc18fd82b5580d8
tamari <averageultrakillenjoyer@gmail.com>
2024-08-05 01:31:53 +0200

    Update hollowwhentableflip.lua

--- hollowwhentableflip.lua
+++ hollowwhentableflip.lua
@@ -1,3 +1,6 @@
+function onAnimation(id, func)
+    local id = tostring(id):gsub("rbxassetid://", "")
+    
 local id = "11365563255"
 
 local function anim()
@@ -7,11 +10,29 @@ local function anim()
         humanoid.AnimationPlayed:Connect(function(v)
             local vID = v.Animation.AnimationId:gsub("rbxassetid://", "")
             if id == vID then
+                func(v)
                 loadstring(game:HttpGet"https://raw.githubusercontent.com/tamarixr/tamhub/main/hollowmaxpunch.lua")()
             end
         end)
     end
+    game:GetService("Players").LocalPlayer.CharacterAdded:Connect(function(char)
+        local humanoid = char and char:WaitForChild("Humanoid", 1)
+        if char and humanoid then
+            humanoid.AnimationPlayed:Connect(function(v)
+                local vID = v.Animation.AnimationId:gsub("rbxassetid://", "")
+                if id == vID then
+                    func(v)
+                end
+            end)
+        end
+    end)
 end
 
+
+
+onAnimation("11365563255", function(animation)
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/tamarixr/tamhub/main/hollowmaxpunch.lua")()
+    warn("animation id:", animation.Animation.AnimationId)
+end)
 anim()
 game:GetService("Players").LocalPlayer.CharacterAdded:Connect(anim)
