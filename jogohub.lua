80042b2084c5922caf096356120ce8c7ece9e6f6
tamari <averageultrakillenjoyer@gmail.com>
2024-08-06 18:59:14 +0200

    Update jogohub.lua

--- jogohub.lua
+++ jogohub.lua
@@ -1,7 +1,7 @@
 local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
 
 local win = DiscordLib:Window("GO/JOHUB")
-local serv = win:Server("GO/JO HUB 2.2 (gd refereence", "")
+local serv = win:Server("GO/JO HUB 2.3", "")
 
 local btns = serv:Channel("Universal")
 
