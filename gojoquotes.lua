d763e03c20b3c1ab53dd7212436801251921dc46
tamari <averageultrakillenjoyer@gmail.com>
2024-08-07 03:11:12 +0200

    Create gojoquotes.lua

---
+++ gojoquotes.lua
@@ -0,0 +1,11 @@
+message = "Sorry, Amanai. I’m not even angry over you right now."
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(2.25)
+message = " I bear no grudge against anyone."
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(2.25)
+message = "It’s just that the world feels so, so wonderful right now."
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(2.25)
+message = "Throughout Heaven and Earth, I alone am the honored one."
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
