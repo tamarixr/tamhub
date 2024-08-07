ec0d861fe9aa4c56d3ff8f1cf8429370522ed759
tamari <averageultrakillenjoyer@gmail.com>
2024-08-07 15:59:14 +0200

    Create superseniorgojo.lua

---
+++ superseniorgojo.lua
@@ -0,0 +1,11 @@
+message = "Reversed grip technique really did save me huh?"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(2.25)
+message = "I'm just edging to do it already!"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(2.25)
+message = "It's going in!"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(2.25)
+message = "Mom and dad, i think im... im the honored one after all!."
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
