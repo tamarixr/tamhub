e552190326bb2f47326faf9f45d4e4019cad32ec
tamari <averageultrakillenjoyer@gmail.com>
2024-08-07 16:21:42 +0200

    Update superred.lua

--- superred.lua
+++ superred.lua
@@ -1,2 +1,2 @@
-message = "Reversal Technique, Shoot up the classroom! "
+message = "Reversal Technique, Shoot up the classroom! See you later alligator."
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
