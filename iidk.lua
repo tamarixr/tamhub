23e3351ab74182d5f2b987f3591ccbd9443ce3c4
tamari <averageultrakillenjoyer@gmail.com>
2024-08-07 03:36:40 +0200

    Update iidk.lua

--- iidk.lua
+++ iidk.lua
@@ -27,5 +27,5 @@ end
 
 
 onAnimation("17275798442", function(animation)
-        loadstring(game:HttpGet"https://raw.githubusercontent.com/tamarixr/tamhub/main/gojoquotes.lua.lua")()
+        loadstring(game:HttpGet"https://raw.githubusercontent.com/tamarixr/tamhub/main/gojoquotes.lua")()
 end)
