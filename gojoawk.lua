ad5ca479889102daa75593f59fb6e7dbdb142377
tamari <averageultrakillenjoyer@gmail.com>
2024-08-07 03:54:45 +0200

    Update gojoawk.lua

--- gojoawk.lua
+++ gojoawk.lua
@@ -1,3 +1,3 @@
-game.Players.LocalPlayer.Character.Humanoid.HipHeight = 40
+game.Players.LocalPlayer.Character.Humanoid.HipHeight = 60
 wait(9)
 game.Players.LocalPlayer.Character.Humanoid.HipHeight = 0
