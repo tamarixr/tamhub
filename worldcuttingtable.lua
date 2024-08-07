b4820ff09b3675c82e9d19cb28c0fbde32316b39
tamari <averageultrakillenjoyer@gmail.com>
2024-08-07 17:39:42 +0200

    Create worldcuttingtable.lua

---
+++ worldcuttingtable.lua
@@ -0,0 +1,11 @@
+message = "Scale of the dragon."
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(1.5)
+message = "Twin meteors."
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(1.5)
+message = "Recoil."
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(2.55)
+message = "World cutting slash."
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
