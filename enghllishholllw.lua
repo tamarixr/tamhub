b1fbd94c4c1cd3eb57d99d0e3270e70b52304f62
tamari <averageultrakillenjoyer@gmail.com>
2024-08-07 16:31:49 +0200

    Create enghllishholllw.lua

---
+++ enghllishholllw.lua
@@ -0,0 +1,11 @@
+message = "Take the amplified and the reversal,"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(1.5)
+message = "Then smash together those two different expressions of infinity,"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(1.5)
+message = "To create and push out imaginary mass."
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(2.5)
+message = "Imaginary technique : Hollow, Purple."
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
