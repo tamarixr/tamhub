f48e29119d8c88f5dd8de4858535a20cfa3b139e
tamari <averageultrakillenjoyer@gmail.com>
2024-08-08 13:56:53 +0200

    Update invistablefli.lua

--- invistablefli.lua
+++ invistablefli.lua
@@ -1,3 +1,3 @@
-game.Players.LocalPlayer.Character.Humanoid.HipHeight = 200
-wait(4)
+game.Players.LocalPlayer.Character.Humanoid.HipHeight = 20
+wait(5.5)
 game.Players.LocalPlayer.Character.Humanoid.HipHeight = 0
