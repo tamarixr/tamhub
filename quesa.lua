4e212e3d134d40b750734d7eb818ce6252f220c0
tamari <averageultrakillenjoyer@gmail.com>
2024-08-10 20:13:33 +0200

    Update quesa.lua

--- quesa.lua
+++ quesa.lua
@@ -3,9 +3,6 @@ local character = player.Character or player.CharacterAdded:Wait()
 
 character:WaitForChild("HumanoidRootPart")
 
-local duration = 1
-local interval = 0.1
-
 local final6 = game.ReplicatedStorage.Resources.KJEffects["KJWallCombo"].FinalImpact.Attachment:Clone()
 final6.Parent = game.Players.LocalPlayer.Character["Torso"]
     for _, child in ipairs(final6:GetChildren()) do
