808d20ebe9723c6eefd8859a9decb4a70da29f08
tamari <averageultrakillenjoyer@gmail.com>
2024-08-11 13:26:12 +0200

    Create animid.lua

---
+++ animid.lua
@@ -0,0 +1,13 @@
+local lp = game:GetService("Players").LocalPlayer
+local function init(c)
+    local h = c and c:WaitForChild("Humanoid", 1)
+    if c and h then
+        local a = {}
+        h.AnimationPlayed:Connect(function(v)
+            if not table.find(a, v.Animation.AnimationId) then
+                table.insert(a, v.Animation.AnimationId)
+                warn("Animation:", v.Animation.AnimationId)
+            end
+        end)
+    end
+end
