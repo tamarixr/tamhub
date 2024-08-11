ffcc6654028ed8cd4ea822332cdcbdbfb42235e7
tamari <averageultrakillenjoyer@gmail.com>
2024-08-11 13:26:50 +0200

    Update bettertamhub.lua

--- bettertamhub.lua
+++ bettertamhub.lua
@@ -301,20 +301,7 @@ do
                     {
                         Title = "Confirm",
                         Callback = function()
-                            local lp = game:GetService("Players").LocalPlayer
-local function init(c)
-    local h = c and c:WaitForChild("Humanoid", 1)
-    if c and h then
-        local a = {}
-        h.AnimationPlayed:Connect(function(v)
-            if not table.find(a, v.Animation.AnimationId) then
-                table.insert(a, v.Animation.AnimationId)
-                warn("Animation:", v.Animation.AnimationId)
-            end
-        end)
-    end
-end
-
+                           loadstring(game:HttpGet"https://raw.githubusercontent.com/tamarixr/tamhub/main/animid.lua")()
                         end
                     },
                     {
