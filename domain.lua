0ad995b0e7f8a7a3182a840b05a54865bca16485
tamari <averageultrakillenjoyer@gmail.com>
2024-08-11 19:06:20 +0200

    Create domain.lua

---
+++ domain.lua
@@ -0,0 +1,5 @@
+message = "DOMAIN EXPANSION :"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(0.5)
+message = "DEATH OF A MILLION FISTS"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
