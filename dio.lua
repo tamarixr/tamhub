d42bd110738c35f3d579ccf8185ba2c8ea42a0c6
tamari <averageultrakillenjoyer@gmail.com>
2024-08-11 13:37:19 +0200

    Create dio.lua

---
+++ dio.lua
@@ -0,0 +1,5 @@
+message = "MUDA MUDA MUDA MUDA MUDA!!!!"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(1.5)
+message = "WRYYYYYYYY!!!!"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
