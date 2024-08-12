3a74b7919f27c75a1addf1899709d99fd58ec56b
tamari <averageultrakillenjoyer@gmail.com>
2024-08-12 14:07:30 +0200

    Update hollowmaxpunch.lua

--- hollowmaxpunch.lua
+++ hollowmaxpunch.lua
@@ -6,6 +6,6 @@ game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageReque
 wait(1.5)
 message = "THE GAP BETWEEN WITHIN AND WITHOUT,"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
-wait(2.55)
+wait(2.35)
 message = "HOLLOW PURPLE"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
