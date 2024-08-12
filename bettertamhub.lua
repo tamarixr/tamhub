2862af8d9cd86de7d6b35290c159445857ff6d8e
tamari <averageultrakillenjoyer@gmail.com>
2024-08-12 18:07:40 +0200

    Update bettertamhub.lua

--- bettertamhub.lua
+++ bettertamhub.lua
@@ -3,8 +3,8 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "TamHub v.4.7.2 ",
-    SubTitle = "RYOKI TENKAI!",
+    Title = "TamHub v.4.7.5 ",
+    SubTitle = "haha im better",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
