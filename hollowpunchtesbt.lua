fc10af108cc6ef3889448f70f3b171a535ded3bf
tamari <averageultrakillenjoyer@gmail.com>
2024-08-12 14:07:40 +0200

    Update hollowpunchtesbt.lua

--- hollowpunchtesbt.lua
+++ hollowpunchtesbt.lua
@@ -6,6 +6,6 @@ game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageReque
 wait(1.5)
 message = "Kiyoshiki..."
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
-wait(2.5)
+wait(2.10)
 message = "Murasaki!"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
