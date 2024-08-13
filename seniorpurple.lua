20268862271cb3530d2550d9eb041ee1033a56ef
tamari <averageultrakillenjoyer@gmail.com>
2024-08-13 23:12:33 +0200

    Create seniorpurple.lua

---
+++ seniorpurple.lua
@@ -0,0 +1,11 @@
+message = "IF I CANT HAVE MY GYATS THEN NO ONE CAN!"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(1.5)
+message = "MAXIMUM OUTPUT! :"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(1.5)
+message = "PURPLE!"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(2.35)
+message = "AIR THIS ƀíţçħ OUT!"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
