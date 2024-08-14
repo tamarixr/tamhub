781a492ac8aca8266b15af1e085e5f19afc47139
tamari <averageultrakillenjoyer@gmail.com>
2024-08-14 15:35:59 +0200

    Update gojoawk.lua

--- gojoawk.lua
+++ gojoawk.lua
@@ -1,5 +1,5 @@
 game.Players.LocalPlayer.Character.Humanoid.HipHeight = 30
-wait(6)
+wait(10)
 Player = "LocalPlayer"
 AnimationId = "18897538537"
 local Anim = Instance.new("Animation")
@@ -7,5 +7,5 @@ Anim.AnimationId = "rbxassetid://"..AnimationId
 local k = game.Players[Player].Character.Humanoid:LoadAnimation(Anim)
 k:Play() --Play the animation
 k:AdjustSpeed(1.5) --Set '1' to any value you want to adjust the animation speed.
-wait(1.5)
+wait(2.5)
 game.Players.LocalPlayer.Character.Humanoid.HipHeight = 0
