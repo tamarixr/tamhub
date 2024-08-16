02528baaf95512a631d16bec6981abd70704c2e0
tamari <averageultrakillenjoyer@gmail.com>
2024-08-16 23:05:52 +0200

    Update domain.lua

--- domain.lua
+++ domain.lua
@@ -1,5 +1,8 @@
-message = "DOMAIN EXPANSION :"
+message = "THE GROUND WILL BURN THE SKY WILL TEAR"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
-wait(0.5)
-message = "DEATH OF A MILLION FISTS"
+wait(0.25)
+message = "WITHIN THIS REALM OF ENDLESS GLORY"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(0.25)
+message = "DOMAIN EXPANSION : CRIMSON PURGATORY"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
