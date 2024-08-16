10a4cc80f82b2b5843ee1c6fa60dfacba0ae82d5
tamari <averageultrakillenjoyer@gmail.com>
2024-08-16 23:26:03 +0200

    Update superseniorgojo.lua

--- superseniorgojo.lua
+++ superseniorgojo.lua
@@ -7,5 +7,13 @@ wait(2.25)
 message = "It's going in!"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
 wait(2.25)
+local music = Instance.new("Sound")
+local id = 17111962824
+music.Volume = 1
+music.PlaybackSpeed = 1
+music.Looped = false
+music.SoundId = "rbxassetid://"..id
+music:Play()
+music.Parent = game:GetService("ReplicatedStorage")
 message = "Mom and dad, i think im... im the honored one after all!."
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
