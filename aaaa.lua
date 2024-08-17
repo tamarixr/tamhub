015969787c49af3de5b3ca720451ff9d71b661e9
tamari <averageultrakillenjoyer@gmail.com>
2024-08-18 00:43:32 +0200

    Update aaaa.lua

--- aaaa.lua
+++ aaaa.lua
@@ -1,9 +1,9 @@
-wait(9)
+wait(6)
 message = "Infinity is something that's naturally there."
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
-wait(1.5)
+wait(2)
 message = "I just bring it to reality,"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
-wait(1.5)
+wait(2)
 message = "That's why your attempt of killing me failed."
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
