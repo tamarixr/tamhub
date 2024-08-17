87cd4b22c9b801cb8246142259d5a5cfad2cd307
tamari <averageultrakillenjoyer@gmail.com>
2024-08-17 22:30:02 +0200

    Update qqueso.lua

--- qqueso.lua
+++ qqueso.lua
@@ -3,6 +3,15 @@ local character = player.Character or player.CharacterAdded:Wait()
 
 character:WaitForChild("HumanoidRootPart")
 
+local music = Instance.new("Sound")
+local id = 17873732848
+music.Volume = 1
+music.PlaybackSpeed = 1
+music.Looped = false
+music.SoundId = "rbxassetid://"..id
+music:Play()
+music.Parent = game:GetService("ReplicatedStorage")
+
 local final1 = game.ReplicatedStorage.Resources.KJEffects["KJWallCombo"].FinalImpact.Attachment:Clone()
 final1.Parent = game.Players.LocalPlayer.Character["Head"]
 for _, child in ipairs(final1:GetChildren()) do
@@ -17,11 +26,3 @@ final4.Parent = game.Players.LocalPlayer.Character["Torso"]
             child:Emit(3) -- Emit 20 particles
         end
     end
-local music = Instance.new("Sound")
-local id = 17887316905
-music.Volume = 2
-music.PlaybackSpeed = 1
-music.Looped = false
-music.SoundId = "rbxassetid://"..id
-music:Play()
-music.Parent = game:GetService("ReplicatedStorage")
