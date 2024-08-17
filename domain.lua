7237448792746a511329c2b7fc0e5169ce3f2467
tamari <averageultrakillenjoyer@gmail.com>
2024-08-17 03:38:26 +0200

    Update domain.lua

--- domain.lua
+++ domain.lua
@@ -5,7 +5,7 @@ message = "WITHIN THIS REALM OF ENDLESS GLORY"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
 wait(0.25)
 local music = Instance.new("Sound")
-local id = 7083916310
+local id = 7817341182
 music.Volume = 1
 music.PlaybackSpeed = 1
 music.Looped = false
