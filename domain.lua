d41ea571268cf6a41e965abbf6c9e63c5495bbed
tamari <averageultrakillenjoyer@gmail.com>
2024-08-17 03:38:12 +0200

    Update domain.lua

--- domain.lua
+++ domain.lua
@@ -1,3 +1,9 @@
+message = "THE GROUND WILL BURN THE SKY WILL TEAR"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(0.25)
+message = "WITHIN THIS REALM OF ENDLESS GLORY"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(0.25)
 local music = Instance.new("Sound")
 local id = 7083916310
 music.Volume = 1
@@ -6,11 +12,5 @@ music.Looped = false
 music.SoundId = "rbxassetid://"..id
 music:Play()
 music.Parent = game:GetService("ReplicatedStorage")
-message = "THE GROUND WILL BURN THE SKY WILL TEAR"
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
-wait(0.25)
-message = "WITHIN THIS REALM OF ENDLESS GLORY"
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
-wait(0.25)
 message = "DOMAIN EXPANSION : CRIMSON PURGATORY"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
