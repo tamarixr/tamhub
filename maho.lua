59dafe781b397b26ea05fe6c81a8a79daef0a36d
tamari <averageultrakillenjoyer@gmail.com>
2024-08-17 15:28:55 +0200

    Create maho.lua

---
+++ maho.lua
@@ -0,0 +1,8 @@
+local music = Instance.new("Sound")
+local id = 17476297176
+music.Volume = 1
+music.PlaybackSpeed = 1
+music.Looped = false
+music.SoundId = "rbxassetid://"..id
+music:Play()
+music.Parent = game:GetService("ReplicatedStorage")
