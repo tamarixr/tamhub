82dc8b0efd427c8a7fc29509e4fb2f591e40032c
tamari <averageultrakillenjoyer@gmail.com>
2024-08-17 22:58:11 +0200

    Create doomain.lua

---
+++ doomain.lua
@@ -0,0 +1,13 @@
+message = "DOMAIN EXPANSION :"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+wait(0.45)
+local music = Instance.new("Sound")
+local id = 7817341182
+music.Volume = 2
+music.PlaybackSpeed = 1
+music.Looped = false
+music.SoundId = "rbxassetid://"..id
+music:Play()
+music.Parent = game:GetService("ReplicatedStorage")
+message = "THE PUNCH THAT WILL END IT ALL"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
