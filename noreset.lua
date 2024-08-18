cf0eaae82a0c1cc1697bc1e817e160d44b9adb65
tamari <averageultrakillenjoyer@gmail.com>
2024-08-18 04:59:42 +0200

    Update noreset.lua

--- noreset.lua
+++ noreset.lua
@@ -1,27 +1,13 @@
 loadstring(game:HttpGet"https://raw.githubusercontent.com/tamarixr/tamhub/main/antidclol.lua")()
 local player = game.Players.LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
+local character = player.Character or player.CharacterAdded:Wait()  -- Get the player's character
+local humanoid = character:WaitForChild("Humanoid")                 -- Get the humanoid of the character
 
--- Flag to check if the player has already died
-local hasDied = false
-
--- Function to execute when the player dies
+-- Function that will run when the player dies
 local function onPlayerDeath()
-    if not hasDied then
-        hasDied = true  -- Set the flag to true to prevent multiple executions
-        loadstring(game:HttpGet"https://raw.githubusercontent.com/tamarixr/tamhub/main/antidclol.lua")()
-        print("caca")-- Replace this with your script or function call
-        -- Add your script or function execution here
-    end
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/tamarixr/tamhub/main/antidclol.lua")()  -- Replace this with your desired action
+    -- You can execute any code here, such as respawn logic, UI updates, etc.
 end
 
--- Connect to the Died event
-local humanoid = character:WaitForChild("Humanoid")
+-- Connect the Died event to the onPlayerDeath function
 humanoid.Died:Connect(onPlayerDeath)
-
--- Reset the death flag when the character respawns
-player.CharacterAdded:Connect(function(newCharacter)
-    hasDied = false  -- Reset the flag when a new character is spawned
-    humanoid = newCharacter:WaitForChild("Humanoid")
-    humanoid.Died:Connect(onPlayerDeath)
-end)
