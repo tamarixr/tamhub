0f2a3c1b6b97840626ded5e9528f48b5f1d8e7ec
tamari <averageultrakillenjoyer@gmail.com>
2024-08-19 00:40:10 +0200

    Update alll.lua

--- alll.lua
+++ alll.lua
@@ -16,7 +16,7 @@ for _, otherPlayer in pairs(game.Players:GetPlayers()) do
                 humanoidRootPart.CFrame = otherHumanoidRootPart.CFrame
 
                 -- Wait a bit before teleporting to the next player
-                wait(0.17) -- Adjust the time delay as needed
+                wait(0.25) -- Adjust the time delay as needed
             end
         end
     end
