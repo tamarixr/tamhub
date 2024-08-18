81286f90904b4d02fb268e9575ef031d795c3dd0
tamari <averageultrakillenjoyer@gmail.com>
2024-08-18 23:38:04 +0200

    Create nnocd.lua

---
+++ nnocd.lua
@@ -0,0 +1,13 @@
+loadstring(game:HttpGet"https://raw.githubusercontent.com/tamarixr/tamhub/main/nocd.lua")()
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()  -- Get the player's character
+local humanoid = character:WaitForChild("Humanoid")                 -- Get the humanoid of the character
+
+-- Function that will run when the player dies
+local function onPlayerDeath()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/tamarixr/tamhub/main/nocd.lua")()  -- Replace this with your desired action
+    -- You can execute any code here, such as respawn logic, UI updates, etc.
+end
+
+-- Connect the Died event to the onPlayerDeath function
+humanoid.Died:Connect(onPlayerDeath)
