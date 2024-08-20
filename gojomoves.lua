5ddc093c5b449a3a587ee7bf624ec138240c21bb
tamari <averageultrakillenjoyer@gmail.com>
2024-08-20 23:24:52 +0200

    Update gojomoves.lua

--- gojomoves.lua
+++ gojomoves.lua
@@ -107,7 +107,7 @@ playerGui.DescendantAdded:Connect(findGuiAndSetText)
 findGuiAndSetText()
  
  
-local animationId = 10468665991 ---- Get normal punch anim id
+local animationId = 10470389827 ---- Get normal punch anim id
  
  
 local player = game.Players.LocalPlayer
@@ -136,7 +136,59 @@ end
  
 local AnimAnim = Instance.new("Animation")
  
-AnimAnim.AnimationId = "rbxassetid://13073745835" ---- Change to repulse anim id
+AnimAnim.AnimationId = "rbxassetid://15020965094" ---- Change to repulse anim id
+ 
+local Anim = Humanoid:LoadAnimation(AnimAnim)
+ 
+ 
+local startTime = 0
+ 
+ 
+Anim:Play()
+ 
+Anim:AdjustSpeed(0)
+ 
+Anim.TimePosition = startTime
+ 
+Anim:AdjustSpeed(1)
+ 
+ 
+    end
+ 
+end
+ 
+humanoid.AnimationPlayed:Connect(onAnimationPlayed) 
+ 
+local animationId = 10470389827 ---- Get normal punch anim id
+ 
+ 
+local player = game.Players.LocalPlayer
+ 
+local character = player.Character or player.CharacterAdded:Wait()
+ 
+local humanoid = character:WaitForChild("Humanoid")
+ 
+ 
+local function onAnimationPlayed(animationTrack)
+ 
+    if animationTrack.Animation.AnimationId == "rbxassetid://" .. animationId then
+ 
+ 
+local p = game.Players.LocalPlayer
+ 
+local Humanoid = p.Character:WaitForChild("Humanoid")
+ 
+ 
+for _, animTrack in pairs(Humanoid:GetPlayingAnimationTracks()) do
+ 
+    animTrack:Stop()
+ 
+end
+ 
+ 
+local AnimAnim = Instance.new("Animation")
+ 
+AnimAnim.AnimationId = "rbxassetid://15020965094" ---- Change to repulse anim id
  
 local Anim = Humanoid:LoadAnimation(AnimAnim)
  
