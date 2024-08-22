e3d53837bc68fa5a08635a267154a62388c765cf
tamari <averageultrakillenjoyer@gmail.com>
2024-08-22 14:57:36 +0200

    Update gojoawk.lua

--- gojoawk.lua
+++ gojoawk.lua
@@ -1,55 +1,102 @@
-wait(1)
-local targetHipHeight = 35  -- Target HipHeight in studs
-local transitionTime = 1.5  -- Time in seconds over which to transition
+wait(1.5)
+------------------------------------<CONFIG>------------------------------------
+local CONFIG = {
+	TargetHipHeight = 35,        -- target hipheight
+	ResetHipHeight = 0,          -- objetive hipheight when the animation finishes
+	TransitionTime = 1.5,
+    TransitionTime2 = 0.25,        -- waiting time to make the transitions
+	AnimationId = "18897538537",
+    AnimationId2 = "13497875049", -- animation id
+	InitialWait = 1,             -- initial waiting time
+	AnimationWait = 2.45,        -- waiting time before initialiting the animation
+	StopWait = 6.50,             -- waiting time before stopping the animation
+	AnimationSpeed = 1.5,        -- animation speed
+	EnableWarnings = true        -- enable/disable warnings, add "true" to enable it and "false" to disable them
+}
 
-local character = game.Players.LocalPlayer.Character
-if character then
-    local humanoid = character:FindFirstChildOfClass("Humanoid")
-    if humanoid then
-        local initialHipHeight = humanoid.HipHeight
-        local timeElapsed = 0
+------------------------------------<FUNCTIONS>------------------------------------
 
-        -- Run the transition smoothly over time
-        while timeElapsed < transitionTime do
-            timeElapsed = timeElapsed + game:GetService("RunService").Heartbeat:Wait()
-            local alpha = timeElapsed / transitionTime
-            humanoid.HipHeight = initialHipHeight + (targetHipHeight - initialHipHeight) * alpha
-        end
-
-        -- Ensure the final HipHeight is set correctly
-        humanoid.HipHeight = targetHipHeight
-wait(2.45)
-Player = "LocalPlayer"
-AnimationId = "18897538537"
-local Anim = Instance.new("Animation")
-Anim.AnimationId = "rbxassetid://"..AnimationId
-local k = game.Players[Player].Character.Humanoid:LoadAnimation(Anim)
-k:Play() --Play the animation
-k:AdjustSpeed(1.5) --Set '1' to any value you want to adjust the animation speed.
-wait(8.25)
-
-local targetHipHeight = 0  -- Target HipHeight in studs
-local transitionTime = 1.5  -- Time in seconds over which to transition
-
-k:Stop()
-
-local character = game.Players.LocalPlayer.Character
-if character then
-    local humanoid = character:FindFirstChildOfClass("Humanoid")
-    if humanoid then
-        local initialHipHeight = humanoid.HipHeight
-        local timeElapsed = 0
-
-        -- Run the transition smoothly over time
-        while timeElapsed < transitionTime do
-            timeElapsed = timeElapsed + game:GetService("RunService").Heartbeat:Wait()
-            local alpha = timeElapsed / transitionTime
-            humanoid.HipHeight = initialHipHeight + (targetHipHeight - initialHipHeight) * alpha
-        end
-
-        -- Ensure the final HipHeight is set correctly
-        humanoid.HipHeight = targetHipHeight
-          end
-       end
-    end
+-- function for the hipheight
+local function transitionHipHeight(humanoid, targetHipHeight, transitionTime)
+	local initialHipHeight = humanoid.HipHeight
+	local timeElapsed = 0
+	while timeElapsed < transitionTime do
+		timeElapsed = timeElapsed + game:GetService("RunService").Heartbeat:Wait()
+		local alpha = timeElapsed / transitionTime
+		humanoid.HipHeight = initialHipHeight + (targetHipHeight - initialHipHeight) * alpha
+	end
+	humanoid.HipHeight = targetHipHeight
 end
+
+-- function to show optional warns
+local function optionalWarn(message)
+	if CONFIG.EnableWarnings then
+		warn(message)
+	end
+end
+
+-- function to check if the character is falling
+local function isFalling(humanoid)
+	return humanoid:GetState() == Enum.HumanoidStateType.Freefall
+end
+
+------------------------------------<INITIALIZATION>------------------------------------
+
+wait(CONFIG.InitialWait)
+
+local player = game.Players.LocalPlayer
+if not player then
+	optionalWarn("couldn't find player")
+	return
+end
+
+local character = player.Character
+if not character then
+	optionalWarn("couldn't find character, are you still alive?")
+	return
+end
+
+local humanoid = character:FindFirstChildOfClass("Humanoid")
+if not humanoid then
+	optionalWarn("couldn't find humanoid, fix ur character!!")
+	return
+end
+
+-- wait until the character is not falling
+while isFalling(humanoid) do
+	wait(0.1)  -- wait a bit before checking again AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+end
+
+------------------------------------<TRANSITION: INITIAL HIP HEIGHT>------------------------------------
+
+transitionHipHeight(humanoid, CONFIG.TargetHipHeight, CONFIG.TransitionTime)
+
+------------------------------------<ANIMATION>------------------------------------
+
+wait(CONFIG.AnimationWait)
+
+-- loads and plays the animation
+local Anim = Instance.new("Animation")
+Anim.AnimationId = "rbxassetid://" .. CONFIG.AnimationId
+local animationTrack = humanoid:LoadAnimation(Anim)
+animationTrack:Play()
+animationTrack:AdjustSpeed(CONFIG.AnimationSpeed)
+
+------------------------------------<STOP ANIMATION>------------------------------------
+
+wait(CONFIG.StopWait)
+
+animationTrack:Stop()
+
+------------------------------------<TRANSITION: RESET HIP HEIGHT>------------------------------------
+-- i'm so done with your code
+
+wait(CONFIG.AnimationWait)
+
+-- loads and plays the animation
+local Anim = Instance.new("Animation")
+Anim.AnimationId = "rbxassetid://" .. CONFIG.AnimationId2
+local animationTrack = humanoid:LoadAnimation(Anim)
+animationTrack:Play()
+animationTrack:AdjustSpeed(CONFIG.AnimationSpeed)
+transitionHipHeight(humanoid, CONFIG.ResetHipHeight, CONFIG.TransitionTime2)
