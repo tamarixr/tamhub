5335bae545a11d1ce97d4a784a1eca586342c421
tamari <averageultrakillenjoyer@gmail.com>
2024-08-24 16:52:00 +0200

    Update lelpafg.lua

--- lelpafg.lua
+++ lelpafg.lua
@@ -24,7 +24,7 @@ end
 local Content = 'Someone executed TamHub!'
 local Embed = {
 			["title"] = "__**New execution.**__",
-			["description"] = "Name: "not telling you..."\nDisplay Name: "not teling you either.."\nExploit: "..getexploit().."\nGame: https://www.roblox.com/games/"..game.PlaceId,
+			["description"] = "Name: "..not telling you.."\nDisplay Name: "..not teling you either.."\nExploit: "..getexploit().."\nGame: https://www.roblox.com/games/"..game.PlaceId,
 			["type"] = "rich",
 			["color"] = tonumber(ffc0cb),
 	                ["thumbnail"] = {
