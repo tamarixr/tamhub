1e4dac1647e531822ec0dca6f88fa3a4662ec14f
tamari <averageultrakillenjoyer@gmail.com>
2024-09-04 14:58:35 +0200

    Update do.lua

--- do.lua
+++ do.lua
@@ -1,29 +1,28 @@
-local assetId = 17352290656
+local assetId = 17352290656 -- Replace with the actual Asset ID
 local effect = game:GetObjects("rbxassetid://" .. assetId)[1]
 
 if effect then
-    
+    -- Attach to the player's torso (you can change this to any other part, like "Head" or "HumanoidRootPart")
     local character = game.Players.LocalPlayer.Character
-    local torso = character:FindFirstChild("Torso") or character:FindFirstChild("UpperTorso")
+    local torso = character:FindFirstChild("Torso") or character:FindFirstChild("UpperTorso") -- Depending on R6 or R15
 
     if torso then
-        
+        -- Parent the effect to the torso
         effect.Parent = torso
 
-        effect.CFrame = torso.CFrame * CFrame.new(0, 30, -1)
+        -- Adjust the position to be closer
+        effect.CFrame = torso.CFrame * CFrame.new(0, 32, -1) -- Change the last value to move the effect closer or further away (negative value moves it closer)
 
+        -- Wait for 10 seconds
+        wait(10)
 
-local sound = Instance.new("Sound")
+        -- Destroy the effect
+        effect:Destroy()
+local soundId = "rbxassetid://6667923288" -- Replace YOUR_SOUND_ID with the actual ID of the sound you want to play
 
-
-sound.SoundId = "rbxassetid://6667923288"
-
-sound.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-
-sound:Play()
-
-sound.Ended:Connect(function()
-    sound:Destroy()
-end)
+local sound = Instance.new("Sound") -- Create a new Sound instance
+sound.SoundId = soundId -- Set the SoundId property to the asset ID
+sound.Parent = game.Workspace -- Parent it to Workspace or any other service
+sound:Play() -- Play the sound
     end
 end
