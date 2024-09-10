deb457dc1e0f3b0a0b8c08fce9bbba522e5f7130
tamari <averageultrakillenjoyer@gmail.com>
2024-09-10 13:24:49 +0200

    Update ddo.lua

--- ddo.lua
+++ ddo.lua
@@ -1,5 +1,11 @@
 local assetId = 17352290656 -- Replace with the actual Asset ID
 local effect = game:GetObjects("rbxassetid://" .. assetId)[1]
+local soundId = "rbxassetid://6667923288" -- Replace YOUR_SOUND_ID with the actual ID of the sound you want to play
+
+local sound = Instance.new("Sound") -- Create a new Sound instance
+sound.SoundId = soundId -- Set the SoundId property to the asset ID
+sound.Parent = game.Workspace -- Parent it to Workspace or any other service
+sound:Play() -- Play the sound
 
 if effect then
     -- Attach to the player's torso (you can change this to any other part, like "Head" or "HumanoidRootPart")
@@ -14,15 +20,9 @@ if effect then
         effect.CFrame = torso.CFrame * CFrame.new(0, 32, -1) -- Change the last value to move the effect closer or further away (negative value moves it closer)
 
         -- Wait for 10 seconds
-        wait(10)
+        wait(17)
 
         -- Destroy the effect
         effect:Destroy()
-local soundId = "rbxassetid://6667923288" -- Replace YOUR_SOUND_ID with the actual ID of the sound you want to play
-
-local sound = Instance.new("Sound") -- Create a new Sound instance
-sound.SoundId = soundId -- Set the SoundId property to the asset ID
-sound.Parent = game.Workspace -- Parent it to Workspace or any other service
-sound:Play() -- Play the sound
     end
 end
