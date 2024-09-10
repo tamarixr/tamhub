360df1e9b4d2ddb1ea36fa7949f3a8ffb148cc8b
tamari <averageultrakillenjoyer@gmail.com>
2024-09-10 13:23:42 +0200

    Update infini.lua

--- infini.lua
+++ infini.lua
@@ -2,3 +2,4 @@ message = "Domain expansion :"
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
 wait(0.45)
 message = "Infinite Void"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
