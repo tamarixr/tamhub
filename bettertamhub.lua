83f09f72428604796126fdfd280d3deab0895f2c
tamari <averageultrakillenjoyer@gmail.com>
2025-04-24 20:33:46 +0200

    Update bettertamhub.lua

--- bettertamhub.lua
+++ bettertamhub.lua
@@ -1,4 +1,10 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tamarixr/tamhub/refs/heads/main/discord.lua"))()
+local Module = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
+
+Module.Prompt({
+    invite = "https://discord.gg/aWfYuJZR9q", -- required
+    name = "TAMHUB" -- optional
+})
+
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
